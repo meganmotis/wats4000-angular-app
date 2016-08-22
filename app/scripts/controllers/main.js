d3b2356c034317e6755fe5a40d8eab0ae2236b4c
Megan Otis <meganmotis@gmail.com>
2016-08-22 13:59:25 -0700

    completed week 6 reqs

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,12 @@
  * Controller of the wats4000AngularAppApp
  */
 angular.module('wats4000AngularAppApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope, current) {
+    $scope.current = current.query();
+   
+    $scope.refreshCurrent = function(){
+       $scope.current = current.query({
+          location: $scope.location
+       });
+    };
   });
---
+++ app/scripts/services/current.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name wats4000AngularAppApp.current
+ * @description
+ * # current
+ * Factory in the wats4000AngularAppApp.
+ */
+angular.module('wats4000AngularAppApp')
+  .factory('current', function ($resource) {
+    // Service logic
+    // ...
+
+    // Public API here
+    return $resource('http://api.openweathermap.org/data/2.5/weather?q=:location&units=imperial&APPID=e4ee4b46e97613602c84924cc1b9f629', {}, {
+      query: {
+        method: 'GET',
+	params: {
+	  location: 'Seattle,us',
+	  bogus: null
+	},
+	isArray:false
+      }
+    });
+  });
